ce73214df4ffec48392a1e0b13e14335b1bec5cd
mrgaolei <gaolei@liheran.com>
2014-05-29 11:15:16 +0800

    Initial Commit

---
+++ grouper/WPGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WPGAppDelegate.h
+//  grouper
+//
+//  Created by 高磊 on 14-5-29.
+//  Copyright (c) 2014年 WholePort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ grouper/WPGViewController.h
@@ -0,0 +1,13 @@
+//
+//  WPGViewController.h
+//  grouper
+//
+//  Created by 高磊 on 14-5-29.
+//  Copyright (c) 2014年 WholePort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPGViewController : UIViewController
+
+@end
